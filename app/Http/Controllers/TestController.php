270ea36e65a01e9064b06a43b1e94c8daea7d72f
Cesar Sarmiento <itsupport3@myopulence.com>
2021-11-19 18:02:50 -0500

    adding first controller

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    //
+    public function testing_address(Request $request)
+    {
+        $address = $request->get('address');
+
+
+        
+    {
+        
+}
